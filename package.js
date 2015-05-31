33c13445544ff855f44e285ff6dd5ced5fd29238
Christopher Stingl <cjstingl@gmail.com>
2015-05-30 21:14:43 -0400

    [CS] Updating fourseven:scss deps version to newest

--- package.js
+++ package.js
@@ -1,13 +1,13 @@
 Package.describe({
   name: "wolves:neat",
   summary: "Meteor 0.9.3+ - A lightweight, semantic grid framework built on top of Bourbon.",
-  version: "2.0.0",
+  version: "2.1.0",
   git: "https://github.com/wolvesio/meteor-neat"
 });
 
 Package.onUse(function(api) {
   api.versionsFrom('METEOR@0.9.3');
-  api.use('fourseven:scss@1.0.0');
+  api.use('fourseven:scss@3.1.1');
   api.addFiles([
     // Helpers
     "neat/_neat-helpers.scss",
